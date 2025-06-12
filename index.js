a53a43db752f0ef4ebf1beb9041212808485a6c5
Christabel Aidoo <christabelaidoo202@gmail.com>
2025-06-12 11:31:44 +0000

    commit all

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bookRoutes = require('./routes/bookRoutes');
 const responseMiddleware = require('./middlewares/responseMiddleware');
 const errorMiddleware = require('./middlewares/errorMiddleware');
@@ -9,9 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(responseMiddleware);
+app.use(cors());
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1/books', bookRoutes);
+
+app.use(responseMiddleware);
 app.use(errorMiddleware);
 
 app.listen(PORT, () => {
